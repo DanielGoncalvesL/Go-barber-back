81d000768bc3e93042671fc0ec70b800f36f8895
Daniel Gonçalves <danielopesgl05@gmail.com>
2021-02-02 18:22:47 -0300

    Criando o metodo List

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,10 +1,33 @@
 import Appointment from "../models/Appointment";
+import { isEqual } from 'date-fns';
 
-class AppointmentsRepository{
+class AppointmentsRepository {
     private appointments: Appointment[];
 
-    constructor(){
+    constructor() {
         this.appointments = [];
     }
+
+    public findByDate(date: Date): Appointment | null {
+        const findAppointment = this.appointments.find(appointment =>
+            isEqual(date, appointment.date),
+        );
+
+        return findAppointment || null;
+    }
+
+    public create(provider: string, date: Date): Appointment {
+        const appointment = new Appointment(provider, date);
+
+        this.appointments.push(appointment);
+
+        return appointment;
+    }
+
+    public all(): Appointment[]{
+        return this.appointments;
+    }
 }
+
+export default AppointmentsRepository;
 
