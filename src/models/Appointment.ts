222ffc3ca5404ce66ca655cc7c4ca76b8b735224
Daniel Gonçalves <danielopesgl05@gmail.com>
2021-02-02 21:44:26 -0300

    Trabalhando com o conceito de DTO

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,13 +1,13 @@
 import { v4 as uuid } from 'uuid';
 
-class Appointment{
+class Appointment {
     id: string;
 
-    provider:string;
-    
+    provider: string;
+
     date: Date;
 
-    constructor(provider: string, date: Date){
+    constructor({ provider, date }: Omit<Appointment, 'id'>) {
         this.id = uuid();
         this.provider = provider;
         this.date = date;
