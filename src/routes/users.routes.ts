8e5f2115355f51847509f6b22a8087706997ad6b
Daniel Gonçalves <danielopesgl05@gmail.com>
2021-02-12 17:45:01 -0300

    Adicionando imagem ao BD

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -5,6 +5,7 @@ import uploadConfig from '../config/upload';
 import CreateUserService from '../service/CreateUserService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../service/UpdateUserAvatarService';
 
 
 const usersRouter = Router();
@@ -23,9 +24,16 @@ usersRouter.post('/', async (request, response) => {
             password
         });
 
-        delete user.password;
+        const userWithoutPassword = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            created_at: user.created_at,
+            updated_at: user.updated_at,
+          };
+      
 
-       return response.json(user);
+       return response.json(userWithoutPassword);
     } catch (err) {
         return response.status(400).json({ error: err.message });
     }
@@ -33,7 +41,18 @@ usersRouter.post('/', async (request, response) => {
 });
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-    return response.json({ok: true});
+    try {
+        const updateUserAvatarService = new UpdateUserAvatarService();
+
+        const user = await updateUserAvatarService.execute({
+            user_id: request.user.id,
+            avatarFileName: request.file.filename,
+        });
+
+        return response.json(user);
+    } catch (error) {
+        return response.status(400).json({error: error.message});
+    }
 });
 
 export default usersRouter;
