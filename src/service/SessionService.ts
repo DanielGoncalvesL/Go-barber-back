61317cf15fa7c8115b3e14029ae6bc58427cffd0
Daniel Gonçalves <danielopesgl05@gmail.com>
2021-02-13 13:56:40 -0300

    Lidando com erros

--- src/service/SessionService.ts
+++ src/service/SessionService.ts
@@ -8,6 +8,8 @@ import {sign, verify} from 'jsonwebtoken';
 
 import authConfig from '../config/auth';
 
+import AppError from '../errors/AppError';
+
 interface Request{
     email: string,
     password: string,
@@ -22,13 +24,13 @@ class SessionService{
         });
 
         if(!user){
-            throw new Error('Incorrect email/password combination');
+            throw new AppError('Incorrect email/password combination', 401);
         }
 
         const passwordMatched = await compare(password, user.password);
 
         if(!passwordMatched){
-            throw new Error('Incorrect email/password combination');
+            throw new AppError('Incorrect email/password combination', 401);
         }
 
         const { secret, expiresIn } = authConfig.jwt; 
