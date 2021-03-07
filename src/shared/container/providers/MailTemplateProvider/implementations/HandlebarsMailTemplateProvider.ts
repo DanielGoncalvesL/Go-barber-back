ae4e697dc74d5c38036e68c12f9b8757ea1b12fb
Daniel Gonçalves <danielopesgl05@gmail.com>
2021-03-07 14:10:10 -0300

    template engine

--- src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
@@ -1,11 +1,13 @@
 import handlebars from 'handlebars';
-
+import fs from 'fs';
 import IParseMailTemplateDTO from '@shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO';
 import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
 
 export default class HandlebarsMailTemplateProvider implements IMailTemplateProvider {
-  public async parse({ template, variables }: IParseMailTemplateDTO): Promise<string> {
-    const parseTemplate = handlebars.compile(template);
+  public async parse({ file, variables }: IParseMailTemplateDTO): Promise<string> {
+    const templateFileContent = await fs.promises.readFile(file, { encoding: 'utf-8' });
+
+    const parseTemplate = handlebars.compile(templateFileContent);
 
     return parseTemplate(variables);
   }
