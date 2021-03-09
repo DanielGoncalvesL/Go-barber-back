671662f7acb06cd0d9eb17265d8c075df3d97609
Daniel Gonçalves <danielopesgl05@gmail.com>
2021-03-09 15:23:02 -0300

    Excluindo horarios antigos

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -2,37 +2,46 @@ import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes
 import ListProviderDayAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
 
 let fakeAppointmentsRepository: FakeAppointmentsRepository;
-let listProviderMonthAvailabilityService: ListProviderDayAvailabilityService;
+let listProviderDayAvailability: ListProviderDayAvailabilityService;
 
 describe('ListProviderDay', () => {
   beforeEach(() => {
     fakeAppointmentsRepository = new FakeAppointmentsRepository();
-    listProviderMonthAvailabilityService = new ListProviderDayAvailabilityService(fakeAppointmentsRepository);
+    listProviderDayAvailability = new ListProviderDayAvailabilityService(
+      fakeAppointmentsRepository,
+    );
   });
 
   it('should be able to list the day availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2021, 4, 20, 8, 0, 0),
+      date: new Date(2021, 4, 20, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2021, 4, 20, 10, 0, 0),
+      date: new Date(2021, 4, 20, 15, 0, 0),
     });
 
-    const availability = await listProviderMonthAvailabilityService.execute({
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => new Date(2021, 4, 20, 11).getTime());
+
+    const availability = await listProviderDayAvailability.execute({
       provider_id: 'user',
-      day: 20,
-      month: 5,
       year: 2021,
+      month: 5,
+      day: 20,
     });
 
-    expect(availability).toEqual(expect.arrayContaining([
-      { hour: 8, available: false },
-      { hour: 9, available: true },
-      { hour: 10, available: false },
-      { hour: 11, available: true },
-    ]))
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { hour: 8, available: false },
+        { hour: 9, available: false },
+        { hour: 10, available: false },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
+      ]),
+    )
   });
 });
