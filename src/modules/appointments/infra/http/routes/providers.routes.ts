782e432a9126e46f127345e5c04a593800254d62
Daniel Gonçalves <danielopesgl05@gmail.com>
2021-03-10 16:30:16 -0300

    validando dados

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import { celebrate, Segments, Joi } from 'celebrate';
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
 import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController';
 import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController';
@@ -12,7 +13,15 @@ const providerDayAvailabilityController = new ProviderDayAvailabilityController(
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
-providersRouter.get('/:provider_id/month-availability', providerMonthAvailabilityController.index);
-providersRouter.get('/:provider_id/day-availability', providerDayAvailabilityController.index);
+providersRouter.get('/:provider_id/month-availability', celebrate({
+  [Segments.PARAMS]: {
+    provider_id: Joi.string().uuid().required(),
+  },
+}), providerMonthAvailabilityController.index);
+providersRouter.get('/:provider_id/day-availability', celebrate({
+  [Segments.PARAMS]: {
+    provider_id: Joi.string().uuid().required(),
+  },
+}), providerDayAvailabilityController.index);
 
 export default providersRouter;
--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import ProfileController from '@modules/users/infra/http/controllers/ProfileController';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -7,7 +8,21 @@ const profileController = new ProfileController();
 
 profileRouter.use(ensureAuthenticated);
 
-profileRouter.put('/', profileController.update);
+profileRouter.put('/', celebrate({
+  [Segments.BODY]: {
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    old_password: Joi.string(),
+    password: Joi.when('old_password', {
+      is: Joi.exist(),
+      then: Joi.required(),
+    }),
+    password_confirmation: Joi.when('password', {
+      is: Joi.exist(),
+      then: Joi.valid(Joi.ref('password')).required(),
+    }),
+  },
+}), profileController.update);
 profileRouter.get('/show', profileController.show);
 
 export default profileRouter;
