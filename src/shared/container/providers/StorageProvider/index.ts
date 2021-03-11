d6e5b1eca4c8c8f463b44cab82c0aa66dcca99ef
Daniel Gonçalves <danielopesgl05@gmail.com>
2021-03-11 14:23:36 -0300

    configurando cache

---
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -0,0 +1,10 @@
+import { container } from 'tsyringe';
+
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
+import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
+
+const providers = {
+  diskStorage: DiskStorageProvider,
+}
+
+container.registerSingleton<IStorageProvider>('StorageProvider', providers.diskStorage);
